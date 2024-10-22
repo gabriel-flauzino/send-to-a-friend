d7c58197a272f421ff3a28c52acc6685cb5a6969
gabriel.flau.pr <gabriel.flau.pr@gmail.com>
2024-10-22 22:22:13 +0000

    svelte 5 migration

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -18,7 +18,7 @@
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      # "vscodevim.vim"
+      "vscodevim.vim"
       "nullxception.cherry-theme"
       "EditorConfig.EditorConfig"
       "esbenp.prettier-vscode"
